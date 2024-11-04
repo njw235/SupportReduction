1e5c1a97238936ef5b54799d81a6f6bf4da2a491
njw235 <njw235@gmail.com>
2024-11-03 20:37:15 -0500

    Added additional test cases besides solely the blasso chain

--- R/TestGridFree.R
+++ R/TestGridFree.R
@@ -24,3 +24,79 @@ for(j in 1:ncol(ch_blasso$x)){
   error1[j] = L2diff_L2Moment(r, m$support, m$weights)
   error2[j] = L2diff_L2Moment(r, support, weight)
 }
+
+avar_mh = c()
+avar_mh2 = c()
+er_mh = c()
+er_mh2 - c()
+
+for(j in 1:ncol(ch_mh$x)){
+  r = autocov(ch_mh$x[,j])
+  dhat = tune_delta(ch_mh$x[,j],5)$delta*0.8
+  
+  m <- SR1(r = r, delta = dhat)
+  avar_mh[j] <- avar(m)
+  measure = julia_call("momentLSmod", r, dhat, 1e-3)
+  
+  support = vector(mode = "numeric", length = length(measure[[1]]) - 1)
+  weight = vector(mode = "numeric", length = length(measure[[2]]) -1)
+  
+  for(i in 1:length(support)){
+    support[i] = measure[[1]][[i]]
+    weight[i] = measure[[2]][[i]]
+  }
+  avar_mh2[j] = asympVariance(weight, support)
+  er_mh[j] = L2diff_L2Moment(r, m$support, m$weights)
+  er_mh2[j] = L2diff_L2Moment(r, support, weight)
+}
+
+avar_pg = c()
+avar_pg2 = c()
+er_pg = c()
+er_pg2 - c()
+for(j in 1:ncol(ch_pg$x)){
+  r = autocov(ch_pg$x[,j])
+  dhat = tune_delta(ch_pg$x[,j],5)$delta*0.8
+  
+  m <- SR1(r = r, delta = dhat)
+  avar_pg[j] <- avar(m)
+  measure = julia_call("momentLSmod", r, dhat, 1e-3)
+  
+  support = vector(mode = "numeric", length = length(measure[[1]]) - 1)
+  weight = vector(mode = "numeric", length = length(measure[[2]]) -1)
+  
+  for(i in 1:length(support)){
+    support[i] = measure[[1]][[i]]
+    weight[i] = measure[[2]][[i]]
+  }
+  avar_pg2[j] = asympVariance(weight, support)
+  er_pg[j] = L2diff_L2Moment(r, m$support, m$weights)
+  er_pg2[j] = L2diff_L2Moment(r, support, weight)
+}
+
+
+
+avar_var = c()
+avar_var2 = c()
+er_var = c()
+er_var2 - c()
+for(j in 1:ncol(ch_var$x)){
+  r = autocov(ch_var$x[,j])
+  dhat = tune_delta(ch_var$x[,j],5)$delta*0.8
+  
+  m <- SR1(r = r, delta = dhat)
+  avar_var[j] <- avar(m)
+  measure = julia_call("momentLSmod", r, dhat, 1e-3)
+  
+  support = vector(mode = "numeric", length = length(measure[[1]]) - 1)
+  weight = vector(mode = "numeric", length = length(measure[[2]]) -1)
+  
+  for(i in 1:length(support)){
+    support[i] = measure[[1]][[i]]
+    weight[i] = measure[[2]][[i]]
+  }
+  avar_var2[j] = asympVariance(weight, support)
+  er_var[j] = L2diff_L2Moment(r, m$support, m$weights)
+  er_var2[j] = L2diff_L2Moment(r, support, weight)
+}
+
