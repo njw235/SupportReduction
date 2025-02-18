f06e445e45d91ed38bea280aaa5d224fefad71b5
njw235 <njw235@gmail.com>
2025-02-18 18:55:40 -0500

    changed error and added thresholding

--- julia/momentLS.jl
+++ julia/momentLS.jl
@@ -218,8 +218,8 @@ end
 
 
 estimate_poly = function(i,r)
-	m = Int(ceil(exp(1+1/exp(1))*log(10^12)))
-	t = Int(floor(2^abs(i) * log(10^12)))
+	m = Int(ceil(exp(1+1/exp(1))*log(10^8)))
+	t = Int(floor(2^abs(i) * log(10^8)))
 		a0 = (1- 2.0^-abs(i))
 	up = min(m-1,t)
 
@@ -231,7 +231,8 @@ estimate_poly = function(i,r)
 		end
 		b[j+1] = (-1)^j * b[j+1]
 	end
-	
+	eps = 10e-16
+	b[abs(b) .< eps] = 0
 	return(b,a0,up)
 end
 
