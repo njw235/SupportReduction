02d56380daea751e795adbf9ceb8f559ca06a4a2
njw235 <njw235@gmail.com>
2025-02-19 00:24:06 -0500

    Added earthmovers idstance

--- julia/Testmomentls.jl
+++ julia/Testmomentls.jl
@@ -1,5 +1,7 @@
 errorsP = zeros(4,5)
 errorsG = zeros(4,5)
+WerrorsP = zeros(4,5)
+WerrorsG = zeros(4,5)
 for N in 2:5
     for j in 1:5
         @rput N
@@ -16,12 +18,18 @@ for N in 2:5
         R"m = SR1(r,dhat)"
         R"errorp = L2diff_L2Moment(r,supp, weight)"
         R"errorg = L2diff_L2Moment(r, m$support, m$weights)"
+        R"mG = wpp(m$support, m$weights)"
+        R"mP = wpp(supp, weight)"
+        R"me = wpp(c(0.5), c(r[1]))"
+        R"errG = wasserstein(me, mG)"
+        R"errP = wasserstein(me,mP)"
         @rget errorp
         @rget errorg
+        @rget errP
+        @rget errG
         errorsP[N-1, j] = errorp
         errorsG[N-1,j] = errorg
+        WerrorsP[N-1,j] = errP
+        WerrorsG[N-1,j] = errG
     end
 end
-
-errorsP
-errorsG
