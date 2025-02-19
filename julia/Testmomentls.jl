ee07b31bdd9979d1c5c7144fe0a4e93be5f48af3
njw235 <njw235@gmail.com>
2025-02-18 20:28:52 -0500

    fixed simulation code

--- julia/Testmomentls.jl
+++ julia/Testmomentls.jl
@@ -1,4 +1,5 @@
-errors = zeros(4,5)
+errorsP = zeros(4,5)
+errorsG = zeros(4,5)
 for N in 2:5
     for j in 1:5
         @rput N
@@ -7,16 +8,20 @@ for N in 2:5
         R"dhat = tune_delta(x,5)$delta*0.8"
         @rget r
         @rget dhat
-        m = momentLSmod(r, dhat, [0.3],[0.0], 1e-8)
+        m = momentLSmod(r, dhat, [0.3],[0.0], 3e-13)
         supp = m[1]
         weight = m[2]
         @rput supp
         @rput weight
         R"m = SR1(r,dhat)"
-        R"errordiff = L2diff_L2Moment(r, m$support, m$weights) - L2diff_L2Moment(r,supp, weight)"
-        @rget errordiff
-        errors[N-1, j] = errordiff
+        R"errorp = L2diff_L2Moment(r,supp, weight)"
+        R"errorg = L2diff_L2Moment(r, m$support, m$weights)"
+        @rget errorp
+        @rget errorg
+        errorsP[N-1, j] = errorp
+        errorsG[N-1,j] = errorg
     end
 end
 
-errors
+errorsP
+errorsG
